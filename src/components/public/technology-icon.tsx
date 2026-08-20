b39016aa2360c1ed747013ee05cbd62928084600
Felipe Micheletti <felipe.micheletti@outlook.com>
2026-08-20 15:20:43 -0300

    feat: adding skills icons

--- src/components/public/technology-icon.tsx
+++ src/components/public/technology-icon.tsx
@@ -1,32 +1,74 @@
 import type { IconType } from "react-icons";
-import { FaAws, FaJava } from "react-icons/fa";
-import { SiDocker, SiFirebase, SiGit, SiGithub, SiJavascript, SiLaravel, SiMongodb, SiMysql, SiNestjs, SiNextdotjs, SiNodedotjs, SiPhp, SiPostgresql, SiPrisma, SiReact, SiSpringboot, SiTailwindcss, SiTypescript, SiVuedotjs } from "react-icons/si";
+import { FaAws, FaCode, FaCodeBranch, FaDatabase, FaJava, FaPython } from "react-icons/fa";
+import { SiAngular, SiAuth0, SiBootstrap, SiCss, SiDocker, SiEslint, SiFirebase, SiGit, SiGithub, SiGithubactions, SiGo, SiGooglecloud, SiHtml5, SiJavascript, SiJest, SiJunit5, SiLaravel, SiMocha, SiMongodb, SiMysql, SiNestjs, SiNextdotjs, SiNodedotjs, SiPhp, SiPostgresql, SiPostman, SiPrisma, SiPytest, SiReact, SiSpringboot, SiSwagger, SiTailwindcss, SiTypescript, SiVuedotjs } from "react-icons/si";
+import { TbApi, TbTestPipe, TbTopologyStar } from "react-icons/tb";
 
 const icons: Record<string, IconType> = {
+	// Cloud / DevOps
 	aws: FaAws,
+	googlecloud: SiGooglecloud,
 	docker: SiDocker,
-	firebase: SiFirebase,
-	git: SiGit,
-	github: SiGithub,
-	java: FaJava,
+	githubactions: SiGithubactions,
+	cicd: TbTopologyStar,
+
+	// Linguagens
+	openjdk: FaJava,
 	javascript: SiJavascript,
+	typescript: SiTypescript,
+	python: FaPython,
+	go: SiGo,
+	php: SiPhp,
+	html: SiHtml5,
+	css: SiCss,
+
+	// Frameworks 
 	laravel: SiLaravel,
-	mongodb: SiMongodb,
-	mysql: SiMysql,
 	nestjs: SiNestjs,
 	nextjs: SiNextdotjs,
-	nextdotjs: SiNextdotjs,
 	nodejs: SiNodedotjs,
-	nodedotjs: SiNodedotjs,
-	php: SiPhp,
-	postgresql: SiPostgresql,
-	prisma: SiPrisma,
 	react: SiReact,
+	angular: SiAngular,
 	springboot: SiSpringboot,
-	tailwindcss: SiTailwindcss,
-	typescript: SiTypescript,
 	vuejs: SiVuedotjs,
-	vuedotjs: SiVuedotjs,
+
+	// UI
+	bootstrap: SiBootstrap,
+	tailwindcss: SiTailwindcss,
+
+	// Banco de Dados
+	mongodb: SiMongodb,
+	mysql: SiMysql,
+	postgresql: SiPostgresql,
+	firebase: SiFirebase,
+	prisma: SiPrisma,
+	sqlserver: FaDatabase,
+
+	// APIs
+	restful: TbApi,
+	swagger: SiSwagger,
+	oauth2: SiAuth0,
+
+	// Testes
+	jest: SiJest,
+	mocha: SiMocha,
+	junit: SiJunit5,
+	pytest: SiPytest,
+	phpunit: SiPhp,
+	husky: FaCodeBranch,
+	eslint: SiEslint,
+	e2e: TbTestPipe,
+	unittest: TbTestPipe,
+	integration: TbTestPipe,
+
+	// Ferramentas
+	git: SiGit,
+	github: SiGithub,
+	postman: SiPostman,
+
+	// Qualidade
+	cleancode: FaCode,
+	solid: FaCode,
+	tdd: TbTestPipe,
 };
 
 function normalizeIconKey(iconKey: string) {
